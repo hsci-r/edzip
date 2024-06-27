7bed7058ebd469aba64cea7e9ff24eb55a2eb826
Eetu Mäkelä <eetu.makela@iki.fi>
2024-06-27 13:54:56 +0300

    bump version, loosen python version limit

--- edzip/sqlite.py
+++ edzip/sqlite.py
@@ -10,6 +10,7 @@ from edzip import ExternalDirectory
 
 T_co = TypeVar('T_co', covariant=True)
 
+
 class _SqliteBackedSequence(Sequence[T_co]):
 
     def __init__(self, con: sqlite3.Connection, table_name: str, entry_number_field: str, fields: str, _len: int, conversion: Callable[[tuple], T_co]):
@@ -32,7 +33,7 @@ class _SqliteBackedSequence(Sequence[T_co]):
             if index.stop is None:
                 return [self.conversion(row) for row in self.con.execute(f"SELECT {self.fields} FROM {self.table_name} WHERE {self.entry_number_field} >= ?", (index.start,)).fetchall()]
             return [self.conversion(row) for row in
-                self.con.execute(f"SELECT {self.fields} FROM {self.table_name} WHERE {self.entry_number_field} BETWEEN ? AND ?", (index.start, index.stop - 1)).fetchall()]
+                    self.con.execute(f"SELECT {self.fields} FROM {self.table_name} WHERE {self.entry_number_field} BETWEEN ? AND ?", (index.start, index.stop - 1)).fetchall()]
         else:
             return self.conversion(
                 self.con.execute(f"SELECT {self.fields} FROM {self.table_name} WHERE {self.entry_number_field} == ?", (index,)).fetchone())
@@ -43,23 +44,25 @@ class _SqliteBackedSequence(Sequence[T_co]):
     def __reversed__(self) -> Iterator[T_co]:
         return map(self.conversion, self.con.execute(f"SELECT {self.fields} FROM {self.table_name} ORDER BY {self.entry_number_field} DESC"))
 
+
 class SQLiteExternalDirectory(ExternalDirectory):
-    def __init__(self, con: sqlite3.Connection, table_name:str = "offsets", entry_number_field: str = "file_number", filename_field: str = "filename", offset_field: str = "header_offset", compressed_size_field: str = "compressed_size"):
+    def __init__(self, con: sqlite3.Connection, table_name: str = "offsets", entry_number_field: str = "file_number", filename_field: str = "filename", offset_field: str = "header_offset", compressed_size_field: str = "compressed_size"):
         self.con = con
         self.table_name = table_name
         self.entry_number_field = entry_number_field
         self.filename_field = filename_field
         self.offset_field = offset_field
         self.compressed_size_field = compressed_size_field
-        self._len = con.execute(f"SELECT COUNT(*) FROM {table_name}").fetchone()[0]
+        self._len = con.execute(
+            f"SELECT COUNT(*) FROM {table_name}").fetchone()[0]
 
     @property
     def len(self) -> int:
         return self._len
-    
+
     def namelist(self) -> Sequence[str]:
         return _SqliteBackedSequence(self.con, self.table_name, self.entry_number_field, self.filename_field, self._len, lambda x: x[0])
-    
+
     def _tuple_to_zinfo(self, tuple) -> ZipInfo:
         zi = ZipInfo(tuple[2])
         zi.compress_size = tuple[1]
@@ -68,20 +71,26 @@ class SQLiteExternalDirectory(ExternalDirectory):
 
     def infolist(self) -> Sequence[ZipInfo]:
         return _SqliteBackedSequence(self.con, self.table_name, self.entry_number_field, f"{self.offset_field},{self.compressed_size_field},{self.filename_field}", self._len, self._tuple_to_zinfo)
-    
+
     def getinfo(self, name: str) -> ZipInfo:
         zi = ZipInfo(name)
-        (zi.header_offset,zi.compress_size) = self.con.execute(f"SELECT {self.offset_field},{self.compressed_size_field} FROM {self.table_name} WHERE {self.filename_field} = ?", (name,)).fetchone()
+        (zi.header_offset, zi.compress_size) = self.con.execute(
+            f"SELECT {self.offset_field},{self.compressed_size_field} FROM {self.table_name} WHERE {self.filename_field} = ?", (name,)).fetchone()
         return zi
+
 
 def create_sqlite_table(con: sqlite3.Connection):
     con.execute("CREATE TABLE offsets (file_number INTEGER PRIMARY KEY, filename TEXT, header_offset INTEGER, compressed_size INTEGER)")
 
+
 def create_sqlite_indexes(con: sqlite3.Connection):
     con.execute("CREATE INDEX idx_offsets_filename ON offsets (filename)")
 
+
 def insert_zipinfo_into_sqlite(con: sqlite3.Connection, file_number: int, filename: str, header_offset: int, compressed_size: int):
-    con.execute("INSERT INTO offsets (file_number, filename, header_offset, compressed_size) VALUES (?,?,?,?)", (file_number, filename, header_offset, compressed_size))
+    con.execute("INSERT INTO offsets (file_number, filename, header_offset, compressed_size) VALUES (?,?,?,?)",
+                (file_number, filename, header_offset, compressed_size))
+
 
 def create_sqlite_directory_from_zip(zipfile: ZipFile, filename: str) -> sqlite3.Connection:
     """Creates, from the given ZipFile, an SQLite database compatible with ZipFileWithExternalSqliteDirectory.
@@ -99,18 +108,20 @@ def create_sqlite_directory_from_zip(zipfile: ZipFile, filename: str) -> sqlite3
     with con:
         create_sqlite_table(con)
     with con:
-        for i, zinfo in enumerate(tqdm(zipfile.infolist(),unit='entr',dynamic_ncols=True)):
-            insert_zipinfo_into_sqlite(con, i, zinfo.filename, zinfo.header_offset, zinfo.compress_size)
+        for i, zinfo in enumerate(tqdm(zipfile.infolist(), unit='entr', dynamic_ncols=True)):
+            insert_zipinfo_into_sqlite(
+                con, i, zinfo.filename, zinfo.header_offset, zinfo.compress_size)
     with con:
         create_sqlite_indexes(con)
     with con:
         con.execute("VACUUM")
     return con
 
+
 @click.command()
 @click.argument("filename")
 @click.argument("sqlite-filename", required=False)
-def main(filename:str, sqlite_filename:Optional[str] = None):
+def cli(filename: str, sqlite_filename: Optional[str] = None):
     if sqlite_filename is None:
         sqlite_filename = filename + ".offsets.sqlite3"
     with ZipFile(filename, 'r') as zf:
@@ -118,4 +129,4 @@ def main(filename:str, sqlite_filename:Optional[str] = None):
 
 
 if __name__ == "__main__":
-    main()
+    cli()
